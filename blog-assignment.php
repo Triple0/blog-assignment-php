b6a0e7ca1dea50f2bdb02ece5c6f653eba331d88
Triple0 <lekan.olapeju@yahoo.com>
2020-05-01 02:29:09 +0100

    Added source to the code

--- blog-assignment.php
+++ blog-assignment.php
@@ -1,4 +1,8 @@
 <?php
+
+/**
+ * Source of Blog Assignment is from the codes and comments in class
+ */
   ini_set( 'display_errors', 1 );
   ini_set( 'display_startup_errors', 1 );
   error_reporting( E_ALL );
@@ -15,16 +19,16 @@
   <h1>Blogs</h1>
   <?php include './includes/navigation.php';; ?>
   <?php
-    // New object instance of "Snacks" class.
+    // New object instance of "blogs" class.
     $blogs = new Blogs( dirname( __FILE__ ) . '/data/articles.json');
    
-    // Output ALL the snacks we found!
+    // Output ALL the blogs we found!
     $blogs->output();
   ?>
   <h2>Find Article by ID</h2>
   <p>The article with id:<?php $id ?> is:</p>
   <?php
-    // Output just the third snack (remember, arrays start at index: 0.)
+    // Output just the third blog (remember, arrays start at index: 0.)
     $blogs->findblogByIndex( 2 );
   ?> -->
 </body>
